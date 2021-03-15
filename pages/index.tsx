6db693c95d2f846085e715feb0cc25cb1c6fd7fd
TakahiroHimi <takahiro.mth.1209@gmail.com>
2021-03-15 23:14:12 +0900

    update: エンドポイントをGitHub GraphQLAPIに変更

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,37 +1,55 @@
 import { GraphQLClient } from "graphql-request";
 import useSWR from "swr";
+import { Repository } from "../types";
 
-const API = "https://countries.trevorblades.com";
+const API = "https://api.github.com/graphql";
 
 type FetchData = {
-  countries: {
-    code: string;
+  repository: {
     name: string;
-  }[];
-};
+  }
+}
 
 function MovieActors() {
-  const client = new GraphQLClient(API, { headers: {} })
+  const client = new GraphQLClient(API, {
+    headers: {
+      "Authorization":
+        "bearer " + process.env.NEXT_PUBLIC_GITHUB_PERSONAL_ACCESSTOKEN,
+    },
+  });
 
   const { data, error } = useSWR<FetchData>(
     `
-    query ExampleQuery {
-      countries {
-        code
+    query GetRepository($repositoryOwner: String!, $repositoryName: String!, $issuesFirst: Int) {
+      repository(owner: $repositoryOwner, name: $repositoryName) {
         name
+        issues(first: $issuesFirst){
+          edges {
+            node {
+              id
+              title
+            }
+          }
+        }
       }
     }
     `,
-    (query) => client.request(query)
+    (query) =>
+      client.request(query, {
+        repositoryOwner: "TakahiroHimi",
+        repositoryName: "NextAuth-sample",
+        issuesFirst: 10,
+      })
   );
 
-  console.log(data);
+  setTimeout(() => {
+    console.log("test", data?.repository.name);
+  }, 5000);
+  
 
   if (error) return <div>failed to load</div>;
   if (!data) return <div>loading...</div>;
-  return data.countries.map((country) => (
-    <li key={country.code}>{country.name}</li>
-  ));
+  return <><p>{data.repository.name}</p></>;
 }
 
 const IndexPage = () => (
