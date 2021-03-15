c4e1ea38e8fd13234413951b3265959260e601b1
TakahiroHimi <takahiro.mth.1209@gmail.com>
2021-03-15 22:18:09 +0900

    add: graphql実行処理追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,41 @@
-import Link from "next/link";
+import { request } from "graphql-request";
+import useSWR from "swr";
+
+const API = "https://countries.trevorblades.com";
+
+type FetchData = {
+  countries: {
+    code: string;
+    name: string;
+  }[];
+};
+
+function MovieActors() {
+  const { data, error } = useSWR<FetchData>(
+    `
+    query ExampleQuery {
+      countries {
+        code
+        name
+      }
+    }
+    `,
+    (query) => request(API, query)
+  );
+
+  console.log(data);
+
+  if (error) return <div>failed to load</div>;
+  if (!data) return <div>loading...</div>;
+  return data.countries.map((country) => (
+    <li key={country.code}>{country.name}</li>
+  ));
+}
 
 const IndexPage = () => (
   <>
     <h1>Hello Next.js 👋</h1>
+    {MovieActors()}
   </>
 );
 
