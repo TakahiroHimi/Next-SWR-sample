d18b65ead59f010372d5cb3d0b291ad62e9ed22c
TakahiroHimi <takahiro.mth.1209@gmail.com>
2021-03-16 23:06:53 +0900

    add: indexページ作成

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,69 +1,14 @@
-import { GraphQLClient } from "graphql-request";
-import useSWR from "swr";
-
-const API = "https://api.github.com/graphql"; // GraphQLエンドポイントのURL
-const repositoryOwner = "octocat";            // 取得するリポジトリ所有者のユーザー名
-const repositoryName = "Hello-World";         // 取得するリポジトリの名前
-const issuesFirst = 100;                      // 取得するIssueの数
-
-type FetchData = {
-  repository: {
-    name: string;
-    issues: {
-      edges: {
-        node: {
-          id: string;
-          title: string;
-        }
-      }[]
-    }
-  }
-}
-
-function getIssues() {
-  const client = new GraphQLClient(API, {
-    headers: {
-      "Authorization":
-        "bearer " + process.env.NEXT_PUBLIC_GITHUB_PERSONAL_ACCESSTOKEN,
-    },
-  });
-
-  const { data, error } = useSWR<FetchData>(
-    `
-    query GetRepository($repositoryOwner: String!, $repositoryName: String!, $issuesFirst: Int) {
-      repository(owner: $repositoryOwner, name: $repositoryName) {
-        name
-        issues(first: $issuesFirst){
-          edges {
-            node {
-              id
-              title
-            }
-          }
-        }
-      }
-    }
-    `,
-    (query) =>
-      client.request(query, {
-        repositoryOwner: repositoryOwner,
-        repositoryName: repositoryName,
-        issuesFirst: issuesFirst,
-      })
-  );
-  
-
-  if (error) return <div>failed to load</div>;
-  if (!data) return <div>loading...</div>;
-  return data.repository.issues.edges.map((issue) => (
-    <li key={issue.node.id}>{issue.node.title}</li>
-  ))
-}
+import Link from "next/link";
 
 const IndexPage = () => (
   <>
-    <h1>{repositoryOwner}/{repositoryName} Issue List</h1>
-    {getIssues()}
+    <Link href={`/countries`}>
+      <a>countries</a>
+    </Link>
+    <br />
+    <Link href={`/issues`}>
+      <a>issues</a>
+    </Link>
   </>
 );
 
---
+++ pages/issues.tsx
@@ -0,0 +1,70 @@
+import { GraphQLClient } from "graphql-request";
+import useSWR from "swr";
+
+const API = "https://api.github.com/graphql"; // GraphQLエンドポイントのURL
+const repositoryOwner = "octocat";            // 取得するリポジトリ所有者のユーザー名
+const repositoryName = "Hello-World";         // 取得するリポジトリの名前
+const issuesFirst = 100;                      // 取得するIssueの数
+
+type FetchData = {
+  repository: {
+    name: string;
+    issues: {
+      edges: {
+        node: {
+          id: string;
+          title: string;
+        }
+      }[]
+    }
+  }
+}
+
+function getIssues() {
+  const client = new GraphQLClient(API, {
+    headers: {
+      "Authorization":
+        "bearer " + process.env.NEXT_PUBLIC_GITHUB_PERSONAL_ACCESSTOKEN,
+    },
+  });
+
+  const { data, error } = useSWR<FetchData>(
+    `
+    query GetRepository($repositoryOwner: String!, $repositoryName: String!, $issuesFirst: Int) {
+      repository(owner: $repositoryOwner, name: $repositoryName) {
+        name
+        issues(first: $issuesFirst){
+          edges {
+            node {
+              id
+              title
+            }
+          }
+        }
+      }
+    }
+    `,
+    (query) =>
+      client.request(query, {
+        repositoryOwner: repositoryOwner,
+        repositoryName: repositoryName,
+        issuesFirst: issuesFirst,
+      })
+  );
+  
+
+  if (error) return <div>failed to load</div>;
+  if (!data) return <div>loading...</div>;
+  return data.repository.issues.edges.map((issue) => (
+    <li key={issue.node.id}>{issue.node.title}</li>
+  ))
+}
+
+const IndexPage = () => (
+  <>
+    <h1>{repositoryOwner}/{repositoryName} Issue List</h1>
+    {getIssues()}
+  </>
+);
+
+export default IndexPage;
